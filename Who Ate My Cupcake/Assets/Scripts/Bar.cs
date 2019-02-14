5d9830f3cd1e45fcdc898307377414b17983513a
Peace <kbarisbicak@hotmail.com>
2019-02-14 14:55:29 +0100

    Did smoothing for the bar

--- Who Ate My Cupcake/Assets/Scripts/Bar.cs
+++ Who Ate My Cupcake/Assets/Scripts/Bar.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class Bar : MonoBehaviour
 {
     Image bar;
-      public GameObject player;
+    public GameObject player;
 
     public float rateOfDeteriation;
     public float decreasePercentage;
     private float lastTime = 0f;
     public float barPercentage;
     public float firstThreshold;
+    public float maxTime;
+    private float timeLeft;
 
     // Start is called before the first frame update
     void Start()
     {
+        timeLeft = maxTime;
         barPercentage = 1f;
         bar = GetComponent<Image>();
     }
@@ -27,8 +30,11 @@ public class Bar : MonoBehaviour
         lastTime += Time.deltaTime;
         if (lastTime > rateOfDeteriation)
         {
-            barPercentage -= decreasePercentage;
-            bar.fillAmount -= decreasePercentage;
+            timeLeft -= .1f;
+            CalculateTime();
+            bar.fillAmount = barPercentage;
+            //barPercentage -= decreasePercentage;
+           // bar.fillAmount -= decreasePercentage;
             lastTime = 0f;
         }
 
@@ -36,7 +42,11 @@ public class Bar : MonoBehaviour
         {
             player.GetComponent<PlayerController>().speed = player.GetComponent<PlayerController>().alteredSpeed;
             player.GetComponent<PlayerController>().fireRate = player.GetComponent<PlayerController>().alteredFireRate;
-            
+
         }
+    }
+    void CalculateTime()
+    {
+        barPercentage = timeLeft / maxTime;
     }
 }
