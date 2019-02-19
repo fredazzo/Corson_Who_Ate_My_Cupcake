451d6c528c95123120c2afaa0eb7b83d13ea6e59
Peace <kbarisbicak@hotmail.com>
2019-02-19 14:05:25 +0100

    Created Boss_Fight scene, and started to work on the boss

---
+++ Who Ate My Cupcake/Assets/Scripts/Boss_Fight/Boss.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    Rigidbody2D body;
+    public GameObject bossShot;
+    public Vector2 firstBulletSpawn;
+    public Vector2 secondBulletSpawn;
+    public Vector2 thirdBulletSpawn;
+    public float healthPoints;
+    private bool hasMoved;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        hasMoved = false;
+    }
+
+    
+    void Update()
+    {
+        if(!hasMoved)
+        {
+            if (Input.GetButtonDown("First_Skill"))
+            {
+                GameObject firstShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.identity);
+                GameObject secondShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.identity);
+                GameObject thirdShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.identity);
+            }
+            if (Input.GetButtonDown("Second_Skill"))
+                Debug.Log("Second skill used");
+            if (Input.GetButtonDown("Third_Skill"))
+                Debug.Log("Third skill used");
+            if (Input.GetButtonDown("Fourth_Skill"))
+                Debug.Log("Fourth skill used");
+        }
+    }
+}
