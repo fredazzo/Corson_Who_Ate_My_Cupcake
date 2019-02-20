5ad8b44ea6f73dfab4af5321e4f509005ca6f569
Fred Azzopardi <fred.azzo1997@live.co.uk>
2019-02-20 16:19:49 +0100

    renamed Fredrik_Scene to Main_Scene. Added fred_testing scene. Added "Health bar"

---
+++ Who Ate My Cupcake/Assets/Scripts/Game_Controlling/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    int healthPoints;
+    public GameObject player;
+
+    public Image health1;
+    public Image health2;
+    public Image health3;
+    public Image health4;
+    public Image health5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthPoints = player.GetComponent<PlayerController>().health;
+
+        if (healthPoints <= 4)
+        {
+            health5.enabled = false;
+            Debug.Log("health5 disabled");
+        }
+        if (healthPoints <= 3)
+        {
+            health4.enabled = false;
+            Debug.Log("health4 disabled");
+        }
+        if (healthPoints <= 2)
+        {
+            health3.enabled = false;
+            Debug.Log("health3 disabled");
+        }
+        if (healthPoints <= 1)
+        {
+            health2.enabled = false;
+            Debug.Log("health2 disabled");
+        }
+        if (healthPoints <= 0)
+        {
+            health1.enabled = false;
+            Debug.Log("health1 disabled");
+        }
+    }
+}
