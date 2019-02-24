3e2113da4d9df8f87b997dd9b7993a84897e230c
Peace <kbarisbicak@hotmail.com>
2019-02-24 23:13:12 +0100

    Shots and batong movement are scripted.

--- Who Ate My Cupcake/Assets/Scripts/Boss_Fight/Boss.cs
+++ Who Ate My Cupcake/Assets/Scripts/Boss_Fight/Boss.cs
@@ -6,57 +6,76 @@ public class Boss : MonoBehaviour
 {
     Rigidbody2D body;
     public GameObject bossShot;
+
+    public float bulletSpeedX;
+    public float downsideBulletSpeedY;
+    public float upsideBulletSpeedY;
+
     public Vector2 firstBulletSpawn;
-    public Vector2 firstBulletSpeed;
+
     public Vector2 secondBulletSpawn;
-    public Vector2 secondBulletSpeed;
+
     public Vector2 thirdBulletSpawn;
-    public Vector2 thirdBulletSpeed;
+
     public Vector3 rotationPoint;
+    public float rotationAngle;
     private float Angle;
     private Transform initialHammerTransform;
     GameObject hammer;
     public float healthPoints;
     private bool hasMoved;
     float testTime;
+    public float bulletTimer;
+    public float bulletTime;
+    bool isShot;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         hasMoved = false;
         hammer = transform.Find("Hammer").gameObject;
+        isShot = false;
     }
 
     
     void Update()
     {
-        testTime += Time.deltaTime;
-        if(!hasMoved)
-        {
-            if (testTime > 5f)
-            {
-                Angle = Vector2.Angle(new Vector3(-1, 0, 0), new Vector3(firstBulletSpeed.x, firstBulletSpeed.y, 0));
-                GameObject firstShot = Instantiate(bossShot, firstBulletSpawn,Quaternion.Euler(0f,0f, Angle));
-                firstShot.GetComponent<Boss_Projectile>().speedX = firstBulletSpeed.x;
-                firstShot.GetComponent<Boss_Projectile>().speedY = firstBulletSpeed.y;
+        hammer.SetActive(true);
+        hammer.transform.RotateAround(rotationPoint, Vector3.forward, rotationAngle);
+    }
+    void firstSkill()
+    {
+        Angle = Vector2.Angle(new Vector3(-1, 0, 0), new Vector3(bulletSpeedX, 0, 0));
+        GameObject firstShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.Euler(0f, 0f, Angle));
+        firstShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        firstShot.GetComponent<Boss_Projectile>().speedY = 0;
 
-                Angle = Vector2.Angle(new Vector3(1, 0, 0), new Vector3(secondBulletSpeed.x, secondBulletSpeed.y, 0));
-                GameObject secondShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.Euler(0f, 0f, Angle));
-                secondShot.GetComponent<Boss_Projectile>().speedX = secondBulletSpeed.x;
-                secondShot.GetComponent<Boss_Projectile>().speedY = secondBulletSpeed.y;
+        Angle = Vector2.Angle(new Vector3(-1, 0, 0), new Vector3(bulletSpeedX, downsideBulletSpeedY, 0));
+        GameObject secondShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.Euler(0f, 0f, Angle));
+        secondShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        secondShot.GetComponent<Boss_Projectile>().speedY = downsideBulletSpeedY;
 
-                Angle = Vector2.Angle(new Vector3(-1, 0, 0), new Vector3(thirdBulletSpeed.x, thirdBulletSpeed.y, 0));
-                GameObject thirdShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.Euler(0f, 0f, Angle));
-                thirdShot.GetComponent<Boss_Projectile>().speedX = thirdBulletSpeed.x;
-                thirdShot.GetComponent<Boss_Projectile>().speedY = thirdBulletSpeed.y;
+        Angle = Vector2.Angle(new Vector3(1, 0, 0), new Vector3(bulletSpeedX, upsideBulletSpeedY, 0));
+        GameObject thirdShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.Euler(0f, 0f, Angle));
+        thirdShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        thirdShot.GetComponent<Boss_Projectile>().speedY = upsideBulletSpeedY;
+    }
 
-                testTime = 0f;
-            }
-            if (Input.GetButtonDown("Second_Skill"))
-                Debug.Log("Second skill used");
-            if (Input.GetButtonDown("Third_Skill"))
-                Debug.Log("Third skill used");
-            if (Input.GetButtonDown("Fourth_Skill"))
-                Debug.Log("Fourth skill used");
-        }
+    IEnumerator secondSkill()
+    {
+        GameObject firstShot = Instantiate(bossShot, firstBulletSpawn, Quaternion.identity);
+        firstShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        firstShot.GetComponent<Boss_Projectile>().speedY = 0;
+
+        yield return new WaitForSeconds(bulletTimer);
+
+        GameObject secondShot = Instantiate(bossShot, secondBulletSpawn, Quaternion.identity);
+        secondShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        secondShot.GetComponent<Boss_Projectile>().speedY = 0;
+
+        yield return new WaitForSeconds(bulletTimer);
+
+        GameObject thirdShot = Instantiate(bossShot, thirdBulletSpawn, Quaternion.identity);
+        thirdShot.GetComponent<Boss_Projectile>().speedX = bulletSpeedX;
+        thirdShot.GetComponent<Boss_Projectile>().speedY = 0;
     }
 }
