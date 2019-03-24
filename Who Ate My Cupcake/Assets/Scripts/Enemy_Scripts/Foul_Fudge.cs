a01a3e98ee4334e53755d11fa105d73268b3260c
Peace <kbarisbicak@hotmail.com>
2019-03-24 16:09:25 +0100

    FF Animations are done

--- Who Ate My Cupcake/Assets/Scripts/Enemy_Scripts/Foul_Fudge.cs
+++ Who Ate My Cupcake/Assets/Scripts/Enemy_Scripts/Foul_Fudge.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class Foul_Fudge : Enemy
 {
     Rigidbody2D body;
+    GameObject player;
+    Animator anim;
+    Vector2 velocity;
     void Start()
     {
-        Source = GetComponent<AudioSource>();
-       
+        isActive = true;
+        anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
     }
+
+    void Update()
+    {
+        if (healthPoints == 0)
+        {
+            isActive = false;
+            Source.clip = deathSound;
+            Source.Play();
+            if (player.GetComponent<PlayerController>().firstLevel)
+                anim.SetTrigger("First_Death");
+            else
+                anim.SetTrigger("Second_Death");
+            velocity = new Vector2(0f, 0f);
+        }
+        if (anim.GetBool("is_Dead"))
+            Destroy(this.gameObject);
+        if (!isActive)
+            GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
 
     void FixedUpdate()
     {
