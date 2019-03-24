65b808b46d8926cf9ea47cbfcd0e7463f5a07c89
Peace <kbarisbicak@hotmail.com>
2019-03-24 23:30:17 +0100

    Credits Scene implemented

---
+++ Who Ate My Cupcake/Assets/Credits_Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits_Exit : MonoBehaviour
+{
+    public float credits_exit_time;
+    void Update()
+    {
+        StartCoroutine(Credits_exit());
+    }
+
+    IEnumerator Credits_exit()
+    {
+        yield return new WaitForSeconds(credits_exit_time);
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
